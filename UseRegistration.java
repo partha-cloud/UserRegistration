b8326908d2652b86a1c2cea67bf9ae9d16ca57ac
partha-cloud <partha11dey@gmail.com>
2021-03-19 20:32:30 +0530

    first case

---
+++ UseRegistration.java
@@ -0,0 +1,25 @@
+package UserRegistration;
+import java.util.Scanner;
+import java.lang.*;
+public class UseRegistration {
+	private static final Scanner scan = new Scanner(System.in);
+    public static  void main(String[] args) {
+    	validateUserData();
+}
+    public static void validateUserData()
+    {
+        System.out.println("Enter Your First name");
+        String name = scan.next();
+        System.out.println("The first name is: " + name);
+        String pattern = "^([A-Z]{1})([a-z]{2,})$";
+
+        if( name.matches(pattern))
+        {
+            System.out.println("Name is Valid");
+        }
+        else
+        {
+            System.out.println("Name is Invalid");
+        }
+    }
+}
